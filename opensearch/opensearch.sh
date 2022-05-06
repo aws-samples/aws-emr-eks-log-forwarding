bf35027b4c3bfa7da996e1488bece96b2f550b7c
tanmatth <79859357+tanmatth@users.noreply.github.com>
2022-05-06 10:26:21 -0400

    Update opensearch.sh

--- opensearch/opensearch.sh
+++ opensearch/opensearch.sh
@@ -4,6 +4,9 @@ export ES_DOMAIN_NAME="emreksdemo"
 # Elasticsearch version
 export ES_VERSION="OpenSearch_1.0"
 
+# Instance Type
+export INSTANCE_TYPE="t3.small.search"
+
 # OpenSearch Dashboards admin user
 export ES_DOMAIN_USER="emreks"
 
@@ -12,10 +15,10 @@ export ES_DOMAIN_PASSWORD='< ADD YOUR PASSWORD >'
 
 
 # Download and update the template using the variables created previously
-curl -sS https://www.eksworkshop.com/intermediate/230_logging/deploy.files/es_domain.json \
+curl -sS https://raw.githubusercontent.com/aws-samples/aws-emr-eks-log-forwarding/main/opensearch/es_domain.json \
   | envsubst > ~/temp/es_domain.json
 
 # Create the cluster
 aws opensearch create-domain \
-  --cli-input-json  file://~/temp/es_domain.json --region us-east-1 --profile cloud9
+  --cli-input-json  file://~/temp/es_domain.json --region ${REGION}
 
